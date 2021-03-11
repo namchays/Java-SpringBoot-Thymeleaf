a4260340e0720ca82170ef7d2b9369c9202ca182
namchays <bautienhoa@gmail.com>
2021-03-11 15:58:21 -0800

    Push

---
+++ src/test/java/com/lenam/springboot/jpa/SpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenam.springboot.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
